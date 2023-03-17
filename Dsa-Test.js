849796ee89600373d41fa02da491b0a9825e7fe6
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-17 12:52:17 +0530

    Das

---
+++ Dsa-Test.js
@@ -0,0 +1,65 @@
+//Q1 
+// var array = [34,45,65,23,5,676,32,34];
+
+// var target = [66];
+   
+
+// var flag = false ;
+
+// for (var i=0; i< array.length ; i++){
+//     if ( vat j = 0; j<array.length; j++){
+//           j+i <= target;
+//        console.log( j,i)
+//     flag = true ;
+//     }  else{
+//         console.log ("not found")
+//     }
+   
+     
+//  }
+
+
+// // Q2 
+// var start = 5768;
+// var end = 78956;  
+
+
+
+
+// //Q3 
+// var number = 19;
+// var array = [3,5,79,45,0,-67,23,77,19,78,12];
+// var flag = false;
+// for (var i=0; i< array.length ; i++){
+//     array[i] === number;
+//     flag = true 
+//     if (flag= true){
+//         console.log ("index")
+//     }
+//     else{
+//         console.log("not found")
+//     }
+
+
+// }
+
+
+
+
+
+
+
+  
+ // Q4 
+ var array = [34,45,567,78,89,34,67,87,34,78,89]
+ for (var i=0; i< array.length ; i++){
+
+    var number = { 0:array[0], 1:array[1], 2:array[2],4:array[3],5:array[4],6:array[5],7:array[6],8:array[7],9:array[8],10:array[9],11:array[10] }
+    console.log (number)
+ }
+
+ 
+
+
+
+
